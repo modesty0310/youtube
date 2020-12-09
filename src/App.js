2348c269e3c9b568e2576bfc1255a75769af9a34
modesty0310 <rlatprua4@gmail.com>
2020-12-09 11:08:59 +0900

    썸네일, 제목, 채널이름 추가 & CSS추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import VideoList from './component/video_list';
+import VideoList from './component/video_list/video_list';
 
 
 function App() {
---
+++ src/component/video_item/video_item.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './video_item.module.css';
+
+const VideoItem = ({video: {snippet}}) => (
+  <div className={styles.containner}>
+    <li className={styles.video}>
+      <img src={snippet.thumbnails.medium.url} alt="video thumbnail" className={styles.thumbnail}/>
+      <div className={styles.metadata}>
+        <p className={styles.title}>{snippet.title}</p>
+        <p className={styles.channel}>{snippet.channelTitle}</p>
+      </div>
+    </li>
+  </div>
+
+  );
+
+export default VideoItem;
