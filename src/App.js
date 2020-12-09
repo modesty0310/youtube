687e2970d35aefae22b8fc4bb637a37906de7ea2
modesty0310 <rlatprua4@gmail.com>
2020-12-09 15:40:44 +0900

    search기능 구현 with postman

--- src/App.js
+++ src/App.js
@@ -7,8 +7,24 @@ import VideoList from './component/video_list/video_list';
 function App() {
   const [videos, setVideos] = useState([]);
 
+
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyCpj-H3ezIxwaL5Ih76t74bhfH8KGq8b34`, requestOptions)
+      .then(response => response.json())
+      .then(result => 
+        result.items.map(item => ({...item, id: item.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
+  
+
   useEffect(() => {
-    var requestOptions = {
+    const requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
@@ -22,7 +38,7 @@ function App() {
 
   return (
     <div className={styles.app}>
-      <SearchHeader />
+      <SearchHeader onSearch={search}/>
       <VideoList videos={videos} />
     </div>
   )
--- src/component/search_header/search_header.jsx
+++ src/component/search_header/search_header.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './search_header.module.css'
 
-const SearchHeader = (props) => (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <img className={styles.img} src="/images/logo.png" alt="logo" />
-        <h1 className={styles.title}>Yuotube</h1>
-      </div>
-      <input className={styles.input} type="search" placeholder="Search..."/>
-      <button className={styles.button} type="submit" >
-        <img className={styles.buttonImg} src="/images/search.png" alt=""/>
-      </button>
-    </header>
-  );
+const SearchHeader = ({onSearch}) => {
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value)
+    inputRef.current.value = ""
+  }
+
+  const onClick = () => {
+    handleSearch()
+  }
+
+  const onKeyPress = (event) => {
+    if(event.key === 'Enter'){
+      handleSearch()
+    }
+    
+  }
+
+  const inputRef = useRef();
+
+  return (    
+  <header className={styles.header}>
+    <div className={styles.logo}>
+      <img className={styles.img} src="/images/logo.png" alt="logo" />
+      <h1 className={styles.title}>Yuotube</h1>
+    </div>
+    <input ref={inputRef} className={styles.input} type="search" placeholder="Search..." onKeyPress={onKeyPress}/>
+    <button className={styles.button} type="submit" onClick={onClick} >
+      <img className={styles.buttonImg} src="/images/search.png" alt="search"/>
+    </button>
+  </header>)
+}
+
+
+  
 
 export default SearchHeader;
