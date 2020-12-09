cf66bda5dc046d5987b3f09defd77d74a4b26d28
modesty0310 <rlatprua4@gmail.com>
2020-12-09 10:07:32 +0900

    유튜브 인기있는 동영상 리스트 제목만 화면에 출력

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import SearchBar from './component/searchBar';
+import VideoList from './component/video_list';
+
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCpj-H3ezIxwaL5Ih76t74bhfH8KGq8b34", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
+
   return (
     <div className="App">
-      <SearchBar />
+      <VideoList videos={videos}/>
     </div>
-  );
+  )
 }
 
 export default App;
